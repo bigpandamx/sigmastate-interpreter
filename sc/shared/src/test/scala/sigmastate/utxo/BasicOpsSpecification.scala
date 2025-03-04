fc5e457ac49d6c2df9a79b29215aa15d71358ce8
novamon <novamon@users.noreply.github.com>
2025-03-04 13:16:26 +0000

    Reapply test fix

--- sc/shared/src/test/scala/sigmastate/utxo/BasicOpsSpecification.scala
+++ sc/shared/src/test/scala/sigmastate/utxo/BasicOpsSpecification.scala
@@ -240,7 +240,7 @@ class BasicOpsSpecification extends CompilerTestingCommons
     )    
 
     test("executeFromSelfReg", customEnv, customExt,
-      "{val ba = executeFromSelfReg[Coll[Byte]](SELF.R4, getVar[Coll[Byte]](defaultVal)); ba.size == 2 }",
+      "{val ba = executeFromSelfReg[Coll[Byte]](4, getVar[Coll[Byte]](defaultVal)); ba.size == 2 }",
       null,
       true,
       additionalRegistersOpt = Some(Map(
