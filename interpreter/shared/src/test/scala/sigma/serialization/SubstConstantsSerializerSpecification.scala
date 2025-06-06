a53f521dfc2219f8346748bc29ff49260f663d41
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2025-06-06 23:18:59 +0300

    fixing tests

--- interpreter/shared/src/test/scala/sigma/serialization/SubstConstantsSerializerSpecification.scala
+++ interpreter/shared/src/test/scala/sigma/serialization/SubstConstantsSerializerSpecification.scala
@@ -1,8 +1,8 @@
 package sigma.serialization
 
-import sigma.ast.{EQ, SInt, SubstConstants}
+import sigma.VersionContext
+import sigma.ast.{ConcreteCollection, EQ, IntArrayConstant, IntConstant, SInt, SubstConstants, Upcast}
 import sigma.ast.syntax.IntValue
-import sigma.ast.{ConcreteCollection, IntArrayConstant, IntConstant}
 import sigma.serialization.ErgoTreeSerializer.DefaultSerializer
 import sigmastate.CrossVersionProps
 
@@ -10,7 +10,12 @@ class SubstConstantsSerializerSpecification extends SerializationSpecification
   with CrossVersionProps {
 
   property("SubstConstant deserialization round trip") {
-    forAll(numExprTreeNodeGen) { prop =>
+    forAll(numExprTreeNodeGen) { propRaw =>
+      val prop = if(VersionContext.current.isV3OrLaterErgoTreeVersion && propRaw.tpe.isInstanceOf[SInt.type]) {
+        Upcast(propRaw, SInt)
+      } else {
+        propRaw
+      }
       val tree = mkTestErgoTree(EQ(prop, IntConstant(1)).toSigmaProp)
       val bytes = DefaultSerializer.serializeErgoTree(tree)
       val newVals = ConcreteCollection(Array[IntValue](1), SInt)
